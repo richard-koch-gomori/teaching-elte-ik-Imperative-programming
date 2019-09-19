30c019ef8160ed7406e0c6f5fb5a313f04ce7a06
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2019-09-19 16:53:39 +0200

    Fix inconsistent indentation in code
    (...)
    modified:   BSc18-2019-20-1/gyakcsop-23/gyak2/uninic.c

--- BSc18-2019-20-1/gyakcsop-23/gyak2/uninic.c
+++ BSc18-2019-20-1/gyakcsop-23/gyak2/uninic.c
@@ -5,9 +5,9 @@
 
 int main()
 {
-	int arr[100];
-	// ar arr tömböt nem inicializáltuk
-	for (int i = 0; i < 100; i++)
+    int arr[100];
+    // ar arr tömböt nem inicializáltuk
+    for (int i = 0; i < 100; i++)
     {
         printf("%i\n", arr[i]);    // néhányszor különbözõ nagyságrendû számokat fog kiírni
     }
