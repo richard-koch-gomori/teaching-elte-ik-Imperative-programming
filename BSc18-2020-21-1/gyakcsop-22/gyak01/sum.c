4c5c409bb14870127e6dc198429d4560063b52d2
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2020-09-10 23:10:36 +0200

    Upload lab1 solutions for group 22
    new file:   BSc18-2020-21-1/gyakcsop-22/gyak01/README.md
    modified:   BSc18-2020-21-1/gyakcsop-22/gyak01/sum.c
    new file:   BSc18-2020-21-1/gyakcsop-22/gyak01/var.c

--- BSc18-2020-21-1/gyakcsop-22/gyak01/sum.c
+++ BSc18-2020-21-1/gyakcsop-22/gyak01/sum.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+
 int main()
 {
     printf("Enter az integer: ");
     int a;
+
+    // scanf()-el olvasunk be a billentyűzetről
+    // egy ilyen scanf() hívás esetén a program végrehajtása megszakad
+    // folytatódik, ha a felhasználó leüti az enter billentyűt
+    // az a változóba kerül az, amit a felhasználó begépelt
     scanf("%i", &a);
+
     printf("Enter an another integer: ");
     int b;
     scanf("%i", &b);
---
+++ BSc18-2020-21-1/gyakcsop-22/gyak01/var.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main() // a program lépéseit írjuk a main()-be, a végrehajtás (programvezérlés) fentről lefelé történik
+{
+    // kiírja a Hello world-ot a képernyőre, \n miatt újsort kezd utána
+    // (n - newline)
+    printf("Hello world\n");
+
+    int x; // x: a változó neve, típusa: int (integer - egész)
+    x = 12; // mivel x típusa int, x-ben egész jellegű értékeket tehetünk
+
+    /*
+    változó: egy változóban a változó típusának megfelelő értékeket tárolhatunk
+    amikor hivatkozunk egy változóra, a benne tárolt pillanatnyi értéket érjük el
+    ha értékadással megváltoztatjuk a változó értékét, akkor eddigi értéke elvész
+    */
+
+    printf("%i\n", x); // a %i (i - int) jelenti, hogy egész jellegű értéket szeretnénk kiírni
+    x = 20;
+
+    // matematikai kifejezéseket is használhatunk
+    x = 40 + 10;
+    x = x + 1;
+
+    printf("%i\n", x);
+
+    return 0;
+}
+
+
