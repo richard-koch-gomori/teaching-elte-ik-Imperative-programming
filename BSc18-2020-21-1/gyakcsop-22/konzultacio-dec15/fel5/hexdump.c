d323d1aef155e630eedff7ae7a4d4bd843c18df9
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2020-12-15 18:53:51 +0100

    Cons/6

---
+++ BSc18-2020-21-1/gyakcsop-22/konzultacio-dec15/fel5/hexdump.c
@@ -0,0 +1,61 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define SIZE 16
+
+void printline(char tomb[], const int size, long int pos)
+{
+    printf("%08lx  ", pos);
+    int i=0;
+    while(i<size){
+        printf("%02x ", tomb[i]);
+        i++;
+    }
+    printf(" | ");
+    i = 0;
+    while(i<size){
+        if (tomb[i] != ' ' && !isgraph(tomb[i]))
+        {
+            printf(". ");
+        }
+        else
+        {
+            printf("%c ", tomb[i]);
+        }
+        i++;
+    }
+    printf("|\n");
+}
+
+
+int hd(FILE* f)
+{
+    char ch;
+    char tomb[SIZE];
+    long int pos = 0;
+    int i=0;
+    do {
+        ch = fgetc(f);
+        if(i<SIZE){
+            tomb[i]=ch;
+            i++;
+        }
+        else{
+            printline(tomb, SIZE, pos);
+            pos += SIZE;
+            i=0;
+        }
+    } while ( ch!= EOF );
+    pos += i;
+    printline(tomb, i, pos);
+    return 0;
+}
+
+int main()
+{
+    return hd(stdin);
+}
+
