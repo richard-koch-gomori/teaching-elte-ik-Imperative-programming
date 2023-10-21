d657443effcbaf2d80315d739f36c82686177e5f
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2023-10-21 23:41:03 +0200

    Add stackoverflow URL about EOF

--- BSc18-2023-24-1/gyakcsop-4/gyak05/char_read.c
+++ BSc18-2023-24-1/gyakcsop-4/gyak05/char_read.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*
+https://stackoverflow.com/questions/11944314/ctrl-d-didnt-stop-the-whilegetchar-eof-loop
+
+EOF is not a character. The EOF is a macro that getchar() returns when it reaches the end of input
+or encounters some kind of error. The ^D is not "an EOF character". What's happening under linux
+when you hit ^D on a line by itself is that it closes the stream, and the getchar() call reaches
+the end of input and returns the EOF macro. If you type ^D somewhere in the middle of a line,
+the stream isn't closed, so getchar() returns values that it read and your loop doesn't exit.
+*/
+
 int main()
 {
 
