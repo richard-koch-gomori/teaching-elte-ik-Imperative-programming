dd3b54e92ab76ed67f9bd1ed5ee9905a52e454d3
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2023-11-10 20:56:09 +0100

    Cons/6

--- BSc18-2023-24-1/konz-nov10/6_prime.c
+++ BSc18-2023-24-1/konz-nov10/6_prime.c
@@ -22,12 +22,47 @@
 // 346347323634747 
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_NUMBERS 10
 
+bool is_prime(int number)
+{
+	for (int i = 2; i < number; i++)
+	{
+		if (number % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool linsearch(int array[], int size, int search)
+{
+    bool found = false;
+    for (int i = 0; i < size; i++)
+    {
+		if (array[i] == search)
+		{
+			found = true;
+			break;
+		}
+	}
+	return found;
+}
+
 int main()
 {
+	//printf("%d\n", is_prime(10));
+	//printf("%d\n", is_prime(11));
+	//printf("%d\n", is_prime(17));
+	//printf("%d\n", is_prime(100));
+
+
+
 	int primes[MAX_NUMBERS];
+	int size = 0;
 	
 	while (1) // végtelen ciklus
 	{
@@ -36,11 +71,28 @@ int main()
 		scanf("%d", &num);
 		
 		// num a szerepel-e primes-ban
-		// ha szerpel, akkor tudjuk h prím
-		// ha nem szerpel
-		//{
-		//    is_prime(num) -> igaz akkor primes-ba betesszük; ha hamis, akkor nem tesszük be
-		//}
+		if (linsearch(primes, size, num))
+		{
+			// tudjuk h num prím
+			printf("%d is a prime number (read from primes array)\n", num);
+		}
+		else
+		{
+			// ha nem szerpel
+			if (is_prime(num))
+			{
+				// primes-ba betesszük num-ot
+				// primes[0] = 7;
+				primes[size] = num;
+				size++;
+				
+				printf("%d is a prime number\n", num);
+			}
+			else
+			{
+				printf("%d is NOT a prime number\n", num);
+			}
+		}
 	}
 }
 
