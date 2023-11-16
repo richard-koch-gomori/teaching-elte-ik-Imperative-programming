56c347668e3d937d90783dc94838db8da0dab7ec
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2023-11-16 20:59:48 +0100

    Cons/9

--- BSc18-2023-24-1/konz-nov16/3.bead/main.c
+++ BSc18-2023-24-1/konz-nov16/3.bead/main.c
@@ -3,7 +3,7 @@
 #define SIZE 10
 #define NUM_OF_SIZES 4
 
-void init(int table[SIZE][SIZE], int num_of_ship[NUM_OF_SIZES]) // TODO hajók számt tároló tömb
+void init(int table[SIZE][SIZE], int num_of_ship[NUM_OF_SIZES])
 {
     for (int i = 0; i < SIZE; ++i)
     {
@@ -63,6 +63,7 @@ int convert_to_idx(char col_ch)
 }
 
 /*
+-4 : hajó sarkokkal ütközünk
 -3 : hajó nem jó hosszú
 -2 : ütközik más hajóval
 -1 : lelóg a pályáról
@@ -103,6 +104,39 @@ int submit(int table[SIZE][SIZE], int num_of_ship[NUM_OF_SIZES],
         printf("row_idx: %d\n", row_idx);
         printf("col_idx: %d\n", col_idx);
 
+        // ha kell legyen köztük 1 üres hely
+        if (row_idx - 1 >= 0 && table[row_idx - 1][col_idx] == 1)
+        {
+            return -2;
+        }
+        if (row_idx + ship_len <= 9 && table[row_idx + ship_len][col_idx] == 1)
+        {
+            return -2;
+        }
+
+        int begin_idx = row_idx - 1;
+        int end_idx = row_idx + 1;
+        if (begin_idx < 0)
+        {
+            begin_idx = 0;
+        }
+        if (end_idx > 9)
+        {
+            end_idx = 9;
+        }
+        for (int i = begin_idx; i <= end_idx; ++i)
+        {
+            if (col_idx - 1 < 0 || col_idx + 1 > 9)
+            {
+                continue;
+            }
+
+            if (table[i][col_idx - 1] == 1 || table[i][col_idx + 1] == 1)
+            {
+                return -4;
+            }
+        }
+
         for (int i = 0; i < ship_len; ++i)
         {
             printf("i = %d\n", i);
@@ -124,11 +158,25 @@ int submit(int table[SIZE][SIZE], int num_of_ship[NUM_OF_SIZES],
                 return -1;
             }
 
+            //table[row_idx + i][col_idx] = 1;
+        }
+        for (int i = 0; i < ship_len; ++i)
+        {
             table[row_idx + i][col_idx] = 1;
         }
     }
     else // vízszintesen
     {
+        // ha kell legyen köztük 1 üres hely
+        if (col_idx - 1  >= 0 && table[row_idx][col_idx - 1] == 1)
+        {
+            return -2;
+        }
+        if (col_idx + ship_len <= 9 && table[row_idx][col_idx + ship_len] == 1 )
+        {
+            return -2;
+        }
+
         for (int i = 0; i < ship_len; ++i)
         {
             if (table[row_idx][col_idx + i] == 1) // ütközik hajóval
@@ -139,16 +187,24 @@ int submit(int table[SIZE][SIZE], int num_of_ship[NUM_OF_SIZES],
             {
                 return -1;
             }
+            //table[row_idx][col_idx + i] = 1;
+        }
+        for (int i = 0; i < ship_len; ++i)
+        {
             table[row_idx][col_idx + i] = 1;
         }
     }
 
     num_of_ship[ship_len - 2]++;
+    return 0;
 }
 
 int main()
 {
     int table[SIZE][SIZE];
+    table[0][30] = 5;
+
+    float x[30][40];
 
     int num_of_ship[NUM_OF_SIZES];
     // idx 0 : 2 méretű hajóból hány darab van
@@ -158,10 +214,20 @@ int main()
 
     init(table, num_of_ship);
     printTable(table);
-    submit(table, num_of_ship, 'C', 4, 3, '|'); // TODO num of ship
+    submit(table, num_of_ship, 'C', 4, 3, '|');
     printTable(table);
-    submit(table, num_of_ship, 'D', 1, 4, '_'); // TODO num of ship
+    submit(table, num_of_ship, 'D', 1, 4, '_');
     printTable(table);
+    printf("RESULT = %d\n", submit(table, num_of_ship, 'J', 1, 3, '|'));
+    printTable(table);
+
+    printf("RESULT = %d\n", submit(table, num_of_ship, 'J', 1, 3, '|'));
+    printf("RESULT = %d\n", submit(table, num_of_ship, 'J', 4, 2, '|'));
+    printf("RESULT = %d\n", submit(table, num_of_ship, 'I', 1, 2, '|'));
+    printf("RESULT = %d\n", submit(table, num_of_ship, 'I', 2, 2, '_'));
+
+    printTable(table);
+    //printf("%d\n", submit(table, num_of_ship, 'D', 1, 4, '_'));
 }
 
 
@@ -169,7 +235,7 @@ int main()
 /* A B C D E F G H I J
 1        1 1 1 1
 2                  1 1 1 1 // nem fér el a pályán
-3  
+3       
 4      1
 5      1
 6      1
