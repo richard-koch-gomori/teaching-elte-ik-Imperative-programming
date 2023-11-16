c5a81f9e4d9386e7bd50c8b78a5fbe9088f2b795
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2023-11-16 19:17:49 +0100

    Cons/3

--- BSc18-2023-24-1/konz-nov16/3.bead/main.c
+++ BSc18-2023-24-1/konz-nov16/3.bead/main.c
@@ -27,6 +27,11 @@ void printTable(int table[SIZE][SIZE])
     printf("-------------------\n");
 }
 
+void submit(int table[SIZE][SIZE], char col_ch, int row, int ship_len, char orientation)
+{
+
+}
+
 int main()
 {
     int table[SIZE][SIZE];
@@ -34,17 +39,18 @@ int main()
 
     init(table);
     printTable(table);
+    submit(table, 'C', 4, 3, '|'); // TODO num of ship
 }
 
 
 // játék:
 /* A B C D E F G H I J
-1
-2
-3
-4
+1  
+2  
+3  
+4      1
 5
-6
+6    
 7
 8
 9
@@ -52,4 +58,16 @@ int main()
 */
 
 // mátrix:
-
+/*
+ 0 1 2 3 4 5 6 7 8 9
+01
+11
+21
+3
+4
+5
+6
+7
+8
+9
+*/
