f575038cb5e684aa8e69d9d789ba20375461954c
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2023-11-16 19:04:43 +0100

    Cons/2

---
+++ BSc18-2023-24-1/konz-nov16/3.bead/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+void init(int table[SIZE][SIZE]) // TODO hajók számt tároló tömb
+{
+    for (int i = 0; i < SIZE; ++i)
+    {
+        for (int j = 0; j < SIZE; ++j)
+        {
+            table[i][j] = 0;
+        }
+    }
+}
+
+void printTable(int table[SIZE][SIZE])
+{
+    printf("-------------------\n");
+    for (int i = 0; i < SIZE; ++i)
+    {
+        for (int j = 0; j < SIZE; ++j)
+        {
+            printf("%d, ", table[i][j]);
+        }
+        printf("\n");
+    }
+    printf("-------------------\n");
+}
+
+int main()
+{
+    int table[SIZE][SIZE];
+    //int num_of_ship // TODO
+
+    init(table);
+    printTable(table);
+}
+
+
+// játék:
+/* A B C D E F G H I J
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
+
+// mátrix:
+
