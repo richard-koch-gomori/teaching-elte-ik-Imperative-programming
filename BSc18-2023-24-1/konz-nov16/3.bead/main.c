1b2f1bd526ea05ff011e51ccbc5ad7a511504fb9
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2023-11-16 20:02:53 +0100

    Cons/8

--- BSc18-2023-24-1/konz-nov16/3.bead/main.c
+++ BSc18-2023-24-1/konz-nov16/3.bead/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 #define SIZE 10
+#define NUM_OF_SIZES 4
 
-void init(int table[SIZE][SIZE]) // TODO hajók számt tároló tömb
+void init(int table[SIZE][SIZE], int num_of_ship[NUM_OF_SIZES]) // TODO hajók számt tároló tömb
 {
     for (int i = 0; i < SIZE; ++i)
     {
@@ -10,6 +11,11 @@ void init(int table[SIZE][SIZE]) // TODO hajók számt tároló tömb
         {
             table[i][j] = 0;
         }
+    }
+
+    for (int i = 0; i < NUM_OF_SIZES; ++i)
+    {
+        num_of_ship[i] = 0;
     }
 }
 
@@ -56,10 +62,35 @@ int convert_to_idx(char col_ch)
     */
 }
 
-void submit(int table[SIZE][SIZE], char col_ch, int row, int ship_len, char orientation)
+/*
+-3 : hajó nem jó hosszú
+-2 : ütközik más hajóval
+-1 : lelóg a pályáról
+*/
+int submit(int table[SIZE][SIZE], int num_of_ship[NUM_OF_SIZES],
+    char col_ch, int row, int ship_len, char orientation)
 {
     int row_idx = row - 1; // 0-tól kezdődik
     int col_idx = convert_to_idx(col_ch); // 0-tól kezdődik ; 'C' -> 3
+
+    if (ship_len < 2 || ship_len > 5)
+    {
+        return -3;
+    }
+    if (ship_len == 3)
+    {
+        if (num_of_ship[1] == 2)
+        {
+            return -3;
+        }
+    }
+    else
+    {
+        if (num_of_ship[ship_len - 2] == 1)
+        {
+            return -3;
+        }
+    }
 
     if (orientation == '|') // függőleges
     {
@@ -83,6 +114,15 @@ void submit(int table[SIZE][SIZE], char col_ch, int row, int ship_len, char orie
             table[5][2] = 1;
             */
 
+            if (table[row_idx + i][col_idx] == 1) // ütközik hajóval
+            {
+                return -2;
+            }
+
+            if (row_idx + i < 0 || row_idx + i > 9 || col_idx < 0 || col_idx > 9)
+            {
+                return -1;
+            }
 
             table[row_idx + i][col_idx] = 1;
         }
@@ -91,21 +131,36 @@ void submit(int table[SIZE][SIZE], char col_ch, int row, int ship_len, char orie
     {
         for (int i = 0; i < ship_len; ++i)
         {
+            if (table[row_idx][col_idx + i] == 1) // ütközik hajóval
+            {
+                return -2;
+            }
+            if (row_idx < 0 || col_idx + i > 9 || row_idx < 0 || col_idx + i > 9)
+            {
+                return -1;
+            }
             table[row_idx][col_idx + i] = 1;
         }
     }
+
+    num_of_ship[ship_len - 2]++;
 }
 
 int main()
 {
     int table[SIZE][SIZE];
-    //int num_of_ship // TODO
 
-    init(table);
+    int num_of_ship[NUM_OF_SIZES];
+    // idx 0 : 2 méretű hajóból hány darab van
+    // idx 1 : 3 méretű hajóból hány darab van
+    // idx 2 : 4 méretű hajóból hány darab van
+    // idx 3 : 5 méretű hajóból hány darab van
+
+    init(table, num_of_ship);
     printTable(table);
-    submit(table, 'C', 4, 3, '|'); // TODO num of ship
+    submit(table, num_of_ship, 'C', 4, 3, '|'); // TODO num of ship
     printTable(table);
-    submit(table, 'D', 1, 4, '_'); // TODO num of ship
+    submit(table, num_of_ship, 'D', 1, 4, '_'); // TODO num of ship
     printTable(table);
 }
 
@@ -113,7 +168,7 @@ int main()
 // játék:
 /* A B C D E F G H I J
 1        1 1 1 1
-2  
+2                  1 1 1 1 // nem fér el a pályán
 3  
 4      1
 5      1
