389ead1c32da279369903e29a0b50eb4dd90459d
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2023-11-30 19:52:05 +0100

    Cons/2

--- BSc18-2023-24-1/konz-nov30/fel1.c
+++ BSc18-2023-24-1/konz-nov30/fel1.c
@@ -1,6 +1,7 @@
-// 11sor/5
+// 11sor/5. feladat
 
 #include <stdio.h>
+#include <string.h>
 
 #define ARRAY_SIZE 256
 
@@ -30,12 +31,31 @@ void conv(char str[], char dest[])
     dest[i] = '\0';
 }
 
-int main()
+// A program parancssori argumentuma legyen a fájlnév
+// amit be kell olvasni
+int main(int argc, char* argv[])
 {
     char array[ARRAY_SIZE] = "van vmi memoriaszemet";
     conv("Hello world", array);
     printf("array = %s\n", array);
 
     //printf("%c\n", 'C' + ('a' - 'A'));
+
+    if (argc < 2)
+    {
+        printf("Usage: a.out [filename]\n");
+        return 1;
+    }
+    printf("The value of argv[1]: %s\n", argv[1]);
+    FILE* textfile = fopen(argv[1], "r"); // "r" - olvasást; // "w" - írás // "a" - a végéhez hozzáírást
+    char line[ARRAY_SIZE];
+    while (fgets(line, ARRAY_SIZE, textfile) != NULL)
+    {
+        line[strlen(line) - 1] = '\0';
+        printf("line = %s\n", line);
+    }
+    fclose(textfile);
+
+    return 0;
 }
 
